158a17bfee3e61534c22885182bcc5e00206a59f
Thomas Qvidahl <tp@live.no>
2021-11-27 00:34:30 +0100

    ⚡️ Better movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,18 @@
 # Author: Thomas Qvidahl <thomas@qvidahl.no>  #
 # Copyright 2021, MIT License                 #
-
-
 extends KinematicBody2D
 
-func _ready():
-	print("Hello World")
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector
+	else:
+		velocity = Vector2.ZERO
+		
+	move_and_collide(velocity)
