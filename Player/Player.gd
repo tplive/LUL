7515a6d1eeab653d6c6f19e8449c7ac052fef0dd
Thomas Qvidahl <tp@live.no>
2021-11-27 12:52:17 +0100

    Code refactor. Introduce states

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,13 @@ const MAX_SPEED = 60
 const ACCELERATION = 500
 const FRICTION = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
@@ -14,8 +21,19 @@ func _ready():
 	animationTree.active = true
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+		ROLL:
+			pass
+
+func attack_state(delta):
+	pass
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
-	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
@@ -28,5 +46,4 @@ func _physics_process(delta):
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-
 	velocity = move_and_slide(velocity)
