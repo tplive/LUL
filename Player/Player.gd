92bba39ccbec574493aec7722d41c3cf7e23e5a7
Thomas Qvidahl <tp@live.no>
2021-11-27 13:11:21 +0100

    Attack animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -30,7 +30,11 @@ func _physics_process(delta):
 			pass
 
 func attack_state(delta):
-	pass
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
@@ -41,9 +45,12 @@ func move_state(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
