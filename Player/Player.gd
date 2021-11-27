033befbbac325fc20523c876160ccf61b0d894a7
Thomas Qvidahl <tp@live.no>
2021-11-27 01:01:53 +0100

    ⚡️ Even more smooooooth

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,8 @@
-# Author: Thomas Qvidahl <thomas@qvidahl.no>  #
-# Copyright 2021, MIT License                 #
 extends KinematicBody2D
 
-const MAX_SPEED = 200
-const ACCELERATION = 25
-const FRICTION = 400
+const MAX_SPEED = 60
+const ACCELERATION = 500
+const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
@@ -16,9 +14,8 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-	move_and_collide(velocity)
+	move_and_collide(velocity * delta)
